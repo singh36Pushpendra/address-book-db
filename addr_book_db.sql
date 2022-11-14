726e947560138b85cc3c9573a0d4e4275d7421f1
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-14 12:32:07 +0530

    Added: UC2 Created table

--- addr_book_db.sql
+++ addr_book_db.sql
@@ -1,2 +1,20 @@
 # creating database for address book service
 create database address_book_service;
+
+# using database
+use address_book_service;
+
+# Creating table
+CREATE TABLE address_book (
+	first_name varchar(20),
+    last_name varchar(20),
+    address varchar(40),
+    city varchar(30),
+    state varchar(40),
+    zip integer,
+    phone_number bigint,
+    email varchar(50)
+);
+
+# Describing table
+desc address_book;
